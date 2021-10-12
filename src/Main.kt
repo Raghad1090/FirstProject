68b8a7ab5f33104ee0c992fb82cd8e0e9ec7e1aa
raghad <RAGHAD.MOHAMMED.ruh.k@tuwaiq.edu.sa>
2021-10-12 15:08:39 +0300

    first commit

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,13 @@
+const val Apples=65
+const val dozens=12
+
 fun main(){
 
-    println("hello world")
+    val numOfDozens=Apples/dozens
+    println("numOfDozens="+numOfDozens)
+
+    val remainAApples=Apples%dozens
+    println("remainAApples="+remainAApples)
 }
 
 
