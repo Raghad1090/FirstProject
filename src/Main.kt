ced5dc12e585bcb4e4c3c3b3a501de6b58c84958
raghad <RAGHAD.MOHAMMED.ruh.k@tuwaiq.edu.sa>
2021-10-12 15:32:17 +0300

    3rd commit

--- src/Main.kt
+++ src/Main.kt
@@ -1,7 +1,15 @@
 fun main(){
 
-    println("hello world")
-}
+    var playerName = "ahmed"
+    var playerHealth = 100
+
+    if (playerHealth == 100) {
+        println(playerName+"is great")
+    } else {
+        println(playerName+"is not great") }
+    }
+
+
 
 
 
